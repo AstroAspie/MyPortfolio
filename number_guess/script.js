2c96f002a9dad533f9b023c0c15c5baff2d5ec6e
CaffinatedJAWA <CaffinatedJawa@users.noreply.github.com>
2020-12-07 02:39:05 +0800

    Update to front-end home and contact pages

--- number_guess/script.js
+++ number_guess/script.js
@@ -2,15 +2,34 @@ let humanScore = 0;
 let computerScore = 0;
 let currentRoundNumber = 1;
 
-function generateTarger() {
+/* Generate target number for guess */
+const generateTarger = () => {
   return Math.floor(Math.random() * 10);
-}
+};
 
-function compareGuesses(human, computer, answer) {
+/* Compare guess to the closest integer of target answer */
+const compareGuesses = (human, computer, answer) => {
   var result = false;
   if ((human === computer || (Math.abs(human) < Math.abs(computer)))) {
     result = true;
   }
   return result;
-}
+};
+
+let updateScore = (winner="") => {
+  switch (winner) {
+    case 'human':
+      humanScore++;
+      break;
+    case 'computer':
+      computerScore++;
+      break;
+    default:
+      console.log('Invalid entry: Acceptable entries \'human\' or \'computer\'');
+  }
+};
+
+const incrementRoundNumber = () => {
+  currentRoundNumber++;
+};
 
