e61b40283a5bc1e44e83ab1607d7e8009ea49933
Alex <45251296+CaffinatedJawa@users.noreply.github.com>
2020-12-14 02:13:00 +0800

    adjustments to directory subtrees to include code_cademy

---
+++ code_cademy_projects/browser_games/number_guess/script.js
@@ -0,0 +1,34 @@
+let humanScore = 0;
+let computerScore = 0;
+let currentRoundNumber = 1;
+
+/* Generate target number for guess */
+const generateTarget = () => {
+  return Math.floor(Math.random() * 10);
+};
+
+/* Compare guess to the closest integer of target answer */
+const compareGuesses = (human, computer, answer) => {
+  var result = false;
+  if ((human === computer || (Math.abs(human) < Math.abs(computer)))) {
+    result = true;
+  }
+  return result;
+};
+
+let updateScore = (winner = "") => {
+  switch (winner) {
+    case 'human':
+      humanScore++;
+      break;
+    case 'computer':
+      computerScore++;
+      break;
+    default:
+      console.log('Invalid entry: Acceptable entries \'human\' or \'computer\'');
+  }
+};
+
+const incrementRoundNumber = () => {
+  currentRoundNumber++;
+};
