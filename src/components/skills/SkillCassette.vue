221bc243f90f9637f92e54cb1f73c3065eb61ea2
Alex <astro.cybersmith@gmail.com>
2024-01-26 01:18:20 +0800

    + skills components

---
+++ src/components/skills/SkillCassette.vue
@@ -0,0 +1,32 @@
+<template>
+  <v-card
+    class="skill-container"
+    max-width="344"
+    outlined
+    tile
+  >
+    <v-card-text>
+      {{ skill_score }}
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+import skillsBanner from "./SkillsBanner.vue";
+
+export default {
+  name: "SkillCassette",
+  props: {
+    skill_score: {
+      type: Number,
+      required: true,
+    },
+  },
+  data() {
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
