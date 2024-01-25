ce65041ea0cddab8a26e583728527176b820541a
Alex <astro.cybersmith@gmail.com>
2024-01-26 00:27:08 +0800

    + project cassette

---
+++ src/components/projects/ProjectCassette.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="project-cassette">
+    <h3>{{ project.title }}</h3>
+    <p>{{ project.description }}</p>
+    <img :src="project.image" alt="project image" />
+    <a :href="project.link" target="_blank">View Project</a>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProjectCassette",
+  props: {
+    project: {
+      type: Object,
+      required: true,
+    }
+  },
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.project-cassette {
+  display: flex;
+  justify-content: space-evenly;
+  text-align: center;
+  align-items: center;
+  height: 4rem;
+}
+</style>
--- src/components/projects/ProjectsBanner.vue
+++ src/components/projects/ProjectsBanner.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="projects">
-    
+    <div class="project-cassette">
+      <ProjectCassette
+        v-for="project in projects"
+        :key="project.title"
+        :project="project"
+      />
+    </div>
   </div>
 </template>
 
@@ -10,7 +16,68 @@ export default {
   name: "ProjectsBanner",
   data() {
     return {
-      projects: [],
+      projects: [
+        {
+          title: "Project 1",
+          description: "This is a description of project 1",
+          image: "https://via.placeholder.com/150",
+          link: "https://www.google.com",
+        },
+        {
+          title: "Project 2",
+          description: "This is a description of project 2",
+          image: "https://via.placeholder.com/150",
+          link: "https://www.google.com",
+        },
+        {
+          title: "Project 3",
+          description: "This is a description of project 3",
+          image: "https://via.placeholder.com/150",
+          link: "https://www.google.com",
+        },
+        {
+          title: "Project 4",
+          description: "This is a description of project 4",
+          image: "https://via.placeholder.com/150",
+          link: "https://www.google.com",
+        },
+        {
+          title: "Project 5",
+          description: "This is a description of project 5",
+          image: "https://via.placeholder.com/150",
+          link: "https://www.google.com",
+        },
+        {
+          title: "Project 6",
+          description: "This is a description of project 6",
+          image: "https://via.placeholder.com/150",
+          link: "https://www.google.com",
+        },
+        {
+          title: "Project 7",
+          description: "This is a description of project 7",
+          image: "https://via.placeholder.com/150",
+          link: "https://www.google.com",
+        },
+        {
+          title: "Project 8",
+          description: "This is a description of project 8",
+          image: "https://via.placeholder.com/150",
+          link: "https://www.google.com",
+        },
+        {
+          title: "Project 9",
+          description: "This is a description of project 9",
+          image: "https://via.placeholder.com/150",
+          link: "https://www.google.com",
+        },
+        {
+          title: "Project 10",
+          description: "This is a description of project 10",
+          image: "https://via.placeholder.com/150",
+          link: "https://www.google.com",
+        }
+      ],
       selected_project: null,
     };
   },
