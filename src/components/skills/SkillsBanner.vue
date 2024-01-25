bcab328894b73440ded08940093c411caabbe7bd
Alex <astro.cybersmith@gmail.com>
2024-01-26 01:18:20 +0800

    + skills components

---
+++ src/components/skills/SkillsBanner.vue
@@ -0,0 +1,24 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+  name: "SkillsBanner",
+  data() {
+    return {
+      skills_list: [
+        { name: 'HTML', score: 98 },
+        { name: 'CSS', score: 90 },
+        { name: 'JavaScript', score: 95 },
+        { name: 'Python', score: 90 },
+      ]
+    };
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
