d6fe1656b6008d3dce04de6b2513a0026d80d23c
Alex <astro.cybersmith@gmail.com>
2024-07-30 04:00:34 +0800

    update projects json

--- src/main.js
+++ src/main.js
@@ -2,21 +2,16 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 
-// import vueSmoothScroll from 'vue3-smooth-scroll'
-
 import App from './App.vue'
 import router from './router'
 import CanvasJsChart from '@canvasjs/vue-charts'
-
-const app = createApp(App)
-// const app  = createApp({
-//     render: ()=>(App)
-// });
 
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+
+const app = createApp(App)
 
 const vuetify = createVuetify({
   components,
