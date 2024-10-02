4e59a6398f5e8f44119f963a390b9bc1664e2fa9
Alex <astro.cybersmith@gmail.com>
2024-10-02 14:27:44 +0800

    update job title

--- src/components/AstroBanner.vue
+++ src/components/AstroBanner.vue
@@ -24,7 +24,7 @@ export default {
   data() {
     return {
       welcome_msg_array: [
-          "Full Stack Web Developer",
+          "Full Stack Developer",
       ],
       welcome_msg: "",
     };
